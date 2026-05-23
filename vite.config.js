a1416b4fa0bc0f319d00ad48ad320387739bf2dd
Albert0 <rosalesalberto217@gmail.com>
2026-05-22 21:31:47 -0600

    Configuración archivo packacge y vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Proyecto-Unidad-5---Aplicaci-n-Web-Full-Stack-con-Vue-3-PHP-y-MySQL-Frontend-/',
+  base: '/Proyecto-U5---FullStack/',
   plugins: [vue()],
   server: { port: 5173 }
 })
