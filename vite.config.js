bfc73bf7cc413527e034c4169a304a280a468649
Albert0 <rosalesalberto217@gmail.com>
2026-05-24 16:01:30 -0600

    Configurando el archivo Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Proyecto-U5---FullStack/',
+  base: '/U5_Web/',
   plugins: [vue()],
   server: { port: 5173 }
 })
